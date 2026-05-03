4e9c28e71556bea80b14d0fe944428d55f4e2119
maxxus <66540504+MaxxusX@users.noreply.github.com>
2026-05-02 21:38:29 -0500

    [evalCss.js]: set style nonce, default to dark
    Signed-off-by: maxxus <66540504+MaxxusX@users.noreply.github.com>

--- src/lib/evalCss.js
+++ src/lib/evalCss.js
@@ -1,82 +1,86 @@
-import toStr from 'licia/toStr'
-import each from 'licia/each'
-import filter from 'licia/filter'
-import isStr from 'licia/isStr'
-import keys from 'licia/keys'
-import kebabCase from 'licia/kebabCase'
-import defaults from 'licia/defaults'
-import themes from './themes'
+import toStr from 'licia/toStr';
+import each from 'licia/each';
+import filter from 'licia/filter';
+import isStr from 'licia/isStr';
+import keys from 'licia/keys';
+import kebabCase from 'licia/kebabCase';
+import defaults from 'licia/defaults';
+import themes from './themes';
 
-let styleList = []
-let scale = 1
+let styleList = [];
+let scale = 1;
 
-let curTheme = themes.Light
+let curTheme = themes.Dark;
 
 const exports = function (css, container) {
-  css = toStr(css)
+  css = toStr(css);
 
   for (let i = 0, len = styleList.length; i < len; i++) {
-    if (styleList[i].css === css) return
+    if (styleList[i].css === css) return;
   }
 
-  container = container || exports.container || document.head
-  const el = document.createElement('style')
+  container = container || exports.container || document.head;
+  const el = document.createElement('style');
+  el.type = 'text/css';
+  const nonce = document.querySelector('style[nonce]')?.nonce;
+  if (nonce && nonce.length > 0) {
+    el.nonce = nonce;
+  }
+  
+  container.appendChild(el);
 
-  el.type = 'text/css'
-  container.appendChild(el)
+  const style = { css, el, container };
+  resetStyle(style);
+  styleList.push(style);
 
-  const style = { css, el, container }
-  resetStyle(style)
-  styleList.push(style)
-
-  return style
+  return style;
 }
 
 exports.setScale = function (s) {
-  scale = s
-  resetStyles()
+  scale = s;
+  resetStyles();
 }
 
 exports.setTheme = function (theme) {
   if (isStr(theme)) {
-    curTheme = themes[theme] || themes.Light
+    curTheme = themes[theme] || themes.Dark;
   } else {
-    curTheme = defaults(theme, themes.Light)
+    curTheme = defaults(theme, themes.Dark);
   }
 
-  resetStyles()
+  resetStyles();
 }
 
-exports.getCurTheme = () => curTheme
+exports.getCurTheme = () => curTheme;
 
-exports.getThemes = () => themes
+exports.getThemes = () => themes;
 
 exports.clear = function () {
-  each(styleList, ({ container, el }) => container.removeChild(el))
-  styleList = []
+  each(styleList, ({ container, el }) => container.removeChild(el));
+  styleList = [];
 }
 
 exports.remove = function (style) {
-  styleList = filter(styleList, (s) => s !== style)
+  styleList = filter(styleList, (s) => s !== style);
 
-  style.container.removeChild(style.el)
+  style.container.removeChild(style.el);
 }
 
 function resetStyles() {
-  each(styleList, (style) => resetStyle(style))
+  each(styleList, (style) => resetStyle(style));
 }
 
 function resetStyle({ css, el }) {
-  css = css.replace(/(\d+)px/g, ($0, $1) => +$1 * scale + 'px')
-  css = css.replace(/_/g, 'eruda-')
-  const _keys = keys(themes.Light)
+  css = css.replace(/(\d+)px/g, ($0, $1) => +$1 * scale + 'px');
+  css = css.replace(/_/g, 'eruda-');
+  const _keys = keys(themes.Light);
   each(_keys, (key) => {
     css = css.replace(
       new RegExp(`var\\(--${kebabCase(key)}\\)`, 'g'),
       curTheme[key]
-    )
-  })
-  el.innerText = css
+    );
+  });
+  el.innerText = css;
 }
 
-export default exports
+export default exports;
